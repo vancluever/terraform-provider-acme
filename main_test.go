19dd3d77d649f4273556b643b94575d76b840ce1
Chris Marchesi <cmarchesi@paybyphone.com>
2017-02-28 09:52:30 -0800

    plugin: Add version test
    Add a quick version test for the plugin version that we want to build
    against. This should check the Terraform code that is vendored.

---
+++ main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/plugin"
+)
+
+// pluginVersionExpected provides the expected plugin version that this release
+// of the plugin is built with.
+const pluginVersionExpected = 2
+
+// TestPluginVersion tests the plugin API version of the vendored Terraform
+// libraries.
+func TestPluginVersion(t *testing.T) {
+	if plugin.Handshake.ProtocolVersion != pluginVersionExpected {
+		t.Fatalf("Expected vendored plugin version to be %d, got %d", pluginVersionExpected, plugin.Handshake.ProtocolVersion)
+	}
+}
