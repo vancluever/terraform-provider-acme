c3c30544f04bb801cf93fa131b557748a2f66670
Chris Marchesi <chrism@vancluevertech.com>
2023-05-24 21:39:21 -0700

    Remove gopls from flake (editors should install it)

--- flake.nix
+++ flake.nix
@@ -12,7 +12,6 @@
         devShell = pkgs.mkShell {
           packages = [
             pkgs.go
-            pkgs.gopls
             pkgs.nixfmt
             pkgs.buf
             pkgs.buf-language-server
