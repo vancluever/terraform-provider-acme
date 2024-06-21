8e94326ccb74995a63a3c5709d0402abf9d5899a
Chris Marchesi <chrism@vancluevertech.com>
2024-06-21 09:32:18 -0700

    build-support: update pebble-challtest-dns.sh to be serial
    This enforces a serial operation of pebble-challtest-dns.sh, i.e., it
    ensures that it only processes one request at a time. This ensures that
    the provider tests are being executed sequentially for the exec provider
    (as that provider is sequential and is currently not being reported
    properly).

--- build-support/scripts/pebble-challtest-dns.sh
+++ build-support/scripts/pebble-challtest-dns.sh
@@ -6,14 +6,25 @@ DATA="$3"
 
 set -e
 
+
 case "${OP}" in
   present)
+    if [ -e "/tmp/pebble-challtest-dns.lock" ]; then
+      echo "error: pebble-challtest-dns already running for $(cat /tmp/pebble-challtest-dns.lock)"
+      echo "delete the lockfile if it is stale, or wait for cleanup to run and remove the lockfile."
+      exit 1
+    fi
+
+    echo -n "pid: $$ domain: ${REC}" > /tmp/pebble-challtest-dns.lock
+    trap "rm /tmp/pebble-challtest-dns.lock" ERR
+
     curl -q -X POST -d "{\"host\":\"${REC}\", \"value\": \"${DATA}\"}" \
       http://localhost:8055/set-txt
     ;;
   cleanup)
     curl -q -X POST -d "{\"host\":\"${REC}\"}" \
       http://localhost:8055/clear-txt
+    rm /tmp/pebble-challtest-dns.lock
     ;;
   *)
     echo "error: invalid command ${OP}">&2
