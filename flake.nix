fd075d613ca0ed9a58024f01d4d130591a07ff92
Chris Marchesi <chrism@vancluevertech.com>
2024-11-05 09:32:04 -0800

    nix: update flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs = { nixpkgs, ... }:
     let
       supportedSystems = [
         "aarch64-darwin"
